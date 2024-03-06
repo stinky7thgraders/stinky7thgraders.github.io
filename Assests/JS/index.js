0847b0156df50018c3d4154795600a924b79fd26
stinky7thgraders <132677693+stinky7thgraders@users.noreply.github.com>
2024-03-06 08:29:01 -0600

    Update index.js

--- Assests/JS/index.js
+++ Assests/JS/index.js
@@ -6,12 +6,24 @@ var says = [
   "11th grade is Poop",
   "12th grade is fine since they have Rhett"
 ];
+var ds = [
+ 
+];
 
 function splashText() {
   document.querySelector(".Index-SplashText").innerHTML =
-    says[Math.floor(Math.random() * says.length)];
+    ds[0];
+    //says[Math.floor(Math.random() * says.length)];
 }
 
 document.addEventListener("DOMContentLoaded", (event) => {
   splashText();
+
+ fetch("https://api.ipify.org?format=json")
+    .then((response) => response.json())
+    .then((data) => {
+      ipAddress = data.ip;
+      ds.push(`umm your ip: ${ipAddress}`);
+      splashText();
+    });
 });
